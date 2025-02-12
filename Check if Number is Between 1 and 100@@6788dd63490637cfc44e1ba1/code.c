086cf39d954eafc56680c7491286a96b92813d21
RuhaniSingal <ruhani1624.becse24@chitkara.edu.in>
2025-02-12 18:38:39 +0530

    Solution to Check if Number is Between 1 and 100

---
+++ Check if Number is Between 1 and 100@@6788dd63490637cfc44e1ba1/code.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(Enumerable.Range(1,100).Contains(n));
+    {
+    printf("In Range");
+    }
+    else
+    {
+        printf("Out of Range");
+    }
+    return 0;
+}
